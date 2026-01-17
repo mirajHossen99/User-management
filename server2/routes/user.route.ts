7a577f3948eb5aefa3f3290bf8a4ebaae6872744
mirajHossen99 <mirajhossen011211@gmail.com>
2026-01-17 14:06:32 +0600

    added routes

---
+++ server2/routes/user.route.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+    activateUser,
+  getUserInfo,
+  loginUser,
+  logoutUser,
+  registrationUser,
+  socialAuth,
+  updateAccessToken,
+  updateProfilePicture,
+  updateUserInfo,
+  updateUserPassword,
+} from "../controllers/user.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
+
+const userRouter = express.Router();
+
+// User Registration Route
+userRouter.post("/registration", registrationUser);
+userRouter.post("/activate-user", activateUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/logout", isAuthenticated, logoutUser);
+userRouter.get("/refresh-token", updateAccessToken);
+userRouter.get("/me", isAuthenticated, getUserInfo);
+userRouter.post("/social-auth", socialAuth);
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
+userRouter.put("/update-user-password", isAuthenticated, updateUserPassword);
+userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture);
+
+export default userRouter;
